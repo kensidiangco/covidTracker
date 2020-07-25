1b5838c26573d08ec04a46603bb90438ad654063
kkenn21 <kensidiangco@gmail.com>
2020-07-26 00:38:05 +0800

    fixfixfix

--- covid/settings.py
+++ covid/settings.py
@@ -131,7 +131,7 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     #Allow all host headers
-    ALLOWED_HOSTS = ['covidTracker.herokuapp.com']
+    ALLOWED_HOSTS = ['covidkentracker.herokuapp.com']
     DEBUG = True
 
     #Static asset configuration
