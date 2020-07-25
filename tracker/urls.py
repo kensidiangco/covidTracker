c3bbc85fca5467f69c6efcbd50b56bfd951f77cf
kkenn21 <kensidiangco@gmail.com>
2020-07-26 01:06:43 +0800

    '??'

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('countries', views.countriesCases, name='countriesCases'),
     path('', views.GlobalCovid, name='Global'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -10,17 +10,20 @@ from django.urls import reverse
 def GlobalCovid(request):
     url = 'https://api.covid19api.com/world/total'
     url2 = 'https://api.covid19api.com/summary'
-    countries = country.objects.all()
+
     rGlobal = requests.get(url).json()
+    covidKey = requests.get(url2).json()
+
+    countries = country.objects.all()
 
     if request.method == "POST":
         cntry = request.POST['country']
-
         country.objects.get_or_create(name=cntry)
 
     try:
         countries = country.objects.all()
         country_data = []
+        
         for c in countries:
             r = requests.get(url2).json()
             i = 0
@@ -57,7 +60,6 @@ def GlobalCovid(request):
         'TotalDeaths' : rGlobal['TotalDeaths'],
         'TotalRecovered' : rGlobal['TotalRecovered'],
     }
-    covidKey = requests.get(url2).json()
 
     form = countryForm()
     context = {
@@ -70,58 +72,3 @@ def GlobalCovid(request):
     return render(request, 'GlobalCovid.html', context)
     
     
-def countriesCases(request):
-    url2 = 'https://api.covid19api.com/summary'
-    
-    if request.method == "POST":
-        cntry = request.POST['country']
-
-        country.objects.get_or_create(name=cntry)
-
-
-
-    try:
-     countries = country.objects.all()
-     
-     country_data = []
-     
-     for c in countries:
-         r = requests.get(url2).json()
-         i = 0
-         while i < len(r['Countries']):
-             i += 1
-             if r['Countries'][i]["Country"] == c.name:
-                 covid = {
-                     'Country': r['Countries'][i]['Country'],
-                     'CountryCode': r['Countries'][i]['CountryCode'],
-                     'NewConfirmed': r['Countries'][i]['NewConfirmed'],
-                     'TotalConfirmed': r['Countries'][i]['TotalConfirmed'],
-                     'NewDeaths': r['Countries'][i]['NewDeaths'],
-                     'TotalDeaths': r['Countries'][i]['TotalDeaths'],
-                     'NewRecovered': r['Countries'][i]['NewRecovered'],
-                     'TotalRecovered': r['Countries'][i]['TotalRecovered'],
-                     'Date': r['Countries'][i]['Date'],
-                 }
-                 country_data.append(covid)
-                 break
-    except:
-        if c.name != r['Countries'][1]['Country']:
-            c.delete()
-            messages.error(request, 'Sorry, unable to fetch %s data.'%(c.name))
-            form = countryForm()
-            context = {
-                'country_data': country_data,
-                'form': form,
-                'covidKey': covidKey
-            }
-            return render(request, 'countriesCases.html', context)
-      
-    covidKey = requests.get(url2).json()
-
-    form = countryForm()
-    context = {
-        'country_data': country_data,
-        'form': form,
-        'covidKey': covidKey
-    }
-    return render(request, 'countriesCases.html', context)
