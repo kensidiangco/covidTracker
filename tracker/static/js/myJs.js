c233d29185a7b96864dcbe1cede890b061b0b416
kkenn21 <kensidiangco@gmail.com>
2020-07-30 03:57:01 +0800

    creating/editing template

--- tracker/static/js/myJs.js
+++ tracker/static/js/myJs.js
@@ -4,19 +4,13 @@ window.scroll({
   behavior: 'smooth'
 });
 
-window.scrollBy({ 
-  top: 100, 
-  left: 0, 
-  behavior: 'smooth' 
-});
-
 
 mybutton = document.getElementById("myBtn");
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
